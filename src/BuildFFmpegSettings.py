927cdd401785a6f34011273a9901e52de026a242
tntwise <tntwise3@gmail.com>
2024-07-03 00:04:31 +0000

    add audio

--- src/BuildFFmpegSettings.py
+++ src/BuildFFmpegSettings.py
@@ -69,6 +69,8 @@ class FFMpegSettings:
                 f"{self.fps * self.interpolateTimes}",
                 "-i",
                 "-",
+                "-i",
+                f"{self.inputFile}",
                 "-c:v",
                 "libx264",
                 f"-crf",
